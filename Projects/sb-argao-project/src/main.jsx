3b5c0442d1ce576f9d1913e928c74d7dc382e524
Rod.El <rhodzr@yahoo.com>
2026-03-01 13:07:10 +0800

    Delete Projects/sb-argao-project directory

--- Projects/sb-argao-project/src/main.jsx
+++
@@ -1,7 +0,0 @@
-// Final Production Entry Point for SB Argao
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-
-// Since we are using the CDN approach for the UI in index.html, 
-// this file simply ensures the React environment is initialized.
-console.log("SB Argao Legislative Information System Initialized");
