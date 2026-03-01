5662cb81fdd11d0c7daf8331730acbf69c81bf21
Rod.El <rhodzr@yahoo.com>
2026-03-01 13:03:41 +0800

    Modernize HTML structure and move custom CSS to src/index.css

---
+++ Projects/sb-argao-project/src/main.jsx
@@ -0,0 +1,7 @@
+// Final Production Entry Point for SB Argao
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+// Since we are using the CDN approach for the UI in index.html, 
+// this file simply ensures the React environment is initialized.
+console.log("SB Argao Legislative Information System Initialized");
